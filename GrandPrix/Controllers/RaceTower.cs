75a959da5b774d99c115e4e64595f10c3c5a9936
PetyoPetkow <Pspetkov13@gmail.com>
2021-03-14 18:17:28 +0200

    Adding controller classes (Engine, RaceTower)  and Factories(DriverFactory, TyreFactory)

---
+++ GrandPrix/Controllers/RaceTower.cs
@@ -0,0 +1,43 @@
+﻿namespace GrandPrix.Controllers
+{
+    using GrandPrix.factories;
+    using GrandPrix.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class RaceTower
+    {
+        public void SetTrackInfo(int lapsNumber, int trackLength)
+        {
+            //TODO: Add some logic here …
+        }
+        public void RegisterDriver(List<string> commandArgs)
+        {
+            DriverFactory.Create(commandArgs);
+        }
+
+        public void DriverBoxes(List<string> commandArgs)
+        {
+            //TODO: Add some logic here …
+        }
+
+        public string CompleteLaps(List<string> commandArgs)
+        {
+            //TODO: Add some logic here …
+            return null;
+        }
+
+        public string GetLeaderboard()
+        {
+            //TODO: Add some logic here …
+            return null;
+        }
+
+        public void ChangeWeather(List<string> commandArgs)
+        {
+            //TODO: Add some logic here …
+        }
+
+    }
+}
---
+++ GrandPrix/factories/DriverFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace GrandPrix.factories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    using GrandPrix.Models;
+    using GrandPrix.Models.Drivers;
+    public class DriverFactory
+    {
+        public Driver Create(List<string> commandArgs)
+        {
+            Driver driver = null;
+            Car car = null;
+            
+
+            string type = commandArgs[0];
+            string name = commandArgs[1];
+            int hp = int.Parse(commandArgs[2]);
+            double fuelAmount = double.Parse(commandArgs[3]);
+            string tyreType = commandArgs[4];
+            double tyreHardness = double.Parse(commandArgs[5]);
+            double grip = double.Parse(commandArgs[6]);
+
+            List<string> tyreArgs = new List<string>();
+            tyreArgs.AddRange(tyreType, tyreHardness, grip);
+
+            switch (type)
+            {
+                case "Aggressive":
+                    if (tyreType=="Hard")
+                    {
+                        TyreFactory();
+                        car = new Car(hp, fuelAmount, )
+                    driver = new AggressiveDriver(name, totalTime, car);
+                    }
+                    
+                    break;
+            }
+        }
+    }
+}
